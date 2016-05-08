66929b4083d6a9daf343414e5e9bffe5d198b1eb
Roland Askew <rolandjamesaskew37@gmail.com>
2016-05-09 11:12:46 +1200

    added todos

--- AddStrip/AddStrip/AddStripForm.cs
+++ AddStrip/AddStrip/AddStripForm.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//TODO: create a test form for creating messages (clickdummy)
+//TODO: create a main form, dummy form with no actions or validation.
+//TODO: create namespace structure, and update files.
+
 namespace AddStrip
 {
     public partial class AddStripForm : Form
